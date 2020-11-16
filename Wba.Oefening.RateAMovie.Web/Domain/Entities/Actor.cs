bfc3ed7d0c8551e3125d391ae835202c0daaa6d5
Mileto Di Marco <45310927+Mileto75@users.noreply.github.com>
2020-11-16 10:55:51 +0100

    Initial commit

---
+++ Wba.Oefening.RateAMovie.Web/Domain/Entities/Actor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wba.Oefening.RateAMovie.Web.Domain.Entities
+{
+    public class Actor
+    {
+        public long Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
---
+++ Wba.Oefening.RateAMovie.Web/Domain/Entities/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wba.Oefening.RateAMovie.Web.Domain.Entities
+{
+    public class Movie
+    {
+        //[Key]
+        public long Id { get; set; }
+
+        //[Required]
+        //[MaxLength(200)]
+        public string Title { get; set; }
+
+        public DateTime? ReleaseDate { get; set; }
+    }
+}
