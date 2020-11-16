7e6343de0614de7921004fb9dd6867f36a0e9179
Mileto Di Marco <mileto.di.marco@howest.be>
2020-11-16 11:03:11 +0100

    Add Onmodelcreating

--- Wba.Oefening.RateAMovie.Web/Data/MovieContext.cs
+++ Wba.Oefening.RateAMovie.Web/Data/MovieContext.cs
@@ -30,58 +30,6 @@ namespace Wba.Oefening.RateAMovie.Web.Data
         {
             base.OnModelCreating(modelBuilder);
 
-            //Configure the User Entity
-            modelBuilder.Entity<User>()
-                .HasKey(u => u.Id);
-
-            modelBuilder.Entity<User>()
-                .Property(u => u.Id)
-                .UseSqlServerIdentityColumn();
-
-            modelBuilder.Entity<User>()
-                .Property(u => u.Username)
-                .IsRequired()
-                .HasMaxLength(100);
-
-            modelBuilder.Entity<User>()
-                .Property(u => u.FirstName)
-                .HasMaxLength(100);
-
-            modelBuilder.Entity<User>()
-                .Property(u => u.LastName)
-                .HasMaxLength(100);
-
-            //Configure the Movie Entity
-            modelBuilder.Entity<Movie>()
-                .HasKey(u => u.Id);
-
-            modelBuilder.Entity<Movie>()
-                .Property(u => u.Id)
-                .UseSqlServerIdentityColumn();
-
-            modelBuilder.Entity<Movie>()
-                .Property(u => u.Title)
-                .IsRequired()
-                .HasMaxLength(200);
-
-            //Configure the Actor Entity
-            modelBuilder.Entity<Actor>()
-                .HasKey(u => u.Id);
-
-            modelBuilder.Entity<Actor>()
-                .Property(u => u.Id)
-                .UseSqlServerIdentityColumn();
-
-            modelBuilder.Entity<Actor>()
-                .Property(u => u.FirstName)
-                .IsRequired()
-                .HasMaxLength(100);
-
-            modelBuilder.Entity<Actor>()
-                .Property(u => u.LastName)
-                .IsRequired()
-                .HasMaxLength(100);
-
             
             
 
